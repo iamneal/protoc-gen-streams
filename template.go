4db7d981ff2664cf98556fd64d5efa23cc0380af
iamneal <iamneal89@gmail.com>
2017-06-18 20:36:29 -0600

    added function to parse bytes out of template and file

--- template.go
+++ template.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"bytes"
+	"fmt"
+	"text/template"
+)
+
+// given a File type as .
+var streamTemplate = `
+
+`
+
+func GetTemplateBytes(f *File) ([]byte, error) {
+	buf := &bytes.Buffer{}
+	temp, err := template.New("stream_impl").Parse(streamTemplate)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing the template: %s", err)
+	}
+	if err = temp.Execute(buf, f); err != nil {
+		return nil, fmt.Errorf("error executing the template on file: %s, err: %s", f, err)
+	}
+	return buf.Bytes(), nil
+}
