76841c946d159044d7b4705b533d5addf37053a3
iamneal <iamneal89@gmail.com>
2017-06-18 21:14:50 -0600

    created basic template

--- template.go
+++ template.go
@@ -8,7 +8,16 @@ import (
 
 // given a File type as .
 var streamTemplate = `
+package {{.Pkg}}
 
+import ({{range $key, val := .Imports}}
+$val "$key"{{end}}
+)
+
+{{range $i, $stream := .Streams}}
+type {{$stream.GetStreamImplName}} struct {}
+{{end}}
+  
 `
 
 func GetTemplateBytes(f *File) ([]byte, error) {
@@ -18,7 +27,7 @@ func GetTemplateBytes(f *File) ([]byte, error) {
 		return nil, fmt.Errorf("error parsing the template: %s", err)
 	}
 	if err = temp.Execute(buf, f); err != nil {
-		return nil, fmt.Errorf("error executing the template on file: %s, err: %s", f, err)
+		return nil, fmt.Errorf("error executing the template on file: %+v, err: %s", f, err)
 	}
 	return buf.Bytes(), nil
 }
